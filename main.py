afd60e3bcbe9cc81bf5c90a42454627788695797
Aniket Saha <aniket_saha28@outlook.com>
2026-03-29 01:59:46 +0530

    Build error fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastmcp import FastMCP
 import os
-import aiosqlite  # Changed: sqlite3 → aiosqlite
+
+# import aiosqlite  # Changed: sqlite3 → aiosqlite
 import tempfile
 
 # Use temporary directory which should be writable
